2f6dadc1e7d06a0d1a83b6ec5b6945c3659ab9d7
Hassan Chaudhary <hassan789@gmail.com>
2024-03-02 16:47:27 -0600

    fix

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,11 @@
-no import streamlit as st
+import streamlit as st
 
 PAGES = {
     "Simple-Screener": "This is the Simple-Screener page.",
     "Data-Explorer": "This is the Data-Explorer page."
 }
 
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+selection = st.sidebar.radio("Navigation:", list(PAGES.keys()))
 page = PAGES[selection]
 st.write(page)
 
