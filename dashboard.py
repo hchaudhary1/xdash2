241bc9959553798b7ce69f6c59676dc99b5ce8fc
Hassan Chaudhary <hassan789@gmail.com>
2024-03-02 16:48:09 -0600

    aider: Refactored code to allow for adding more code for each page in separate functions.
    # Aider chat conversation:
    
    USER: I need to add a lot more code for each of these pages. refactor this so that i can add more lines of code for each page in different sections of the file
    
    ASSISTANT: ```diff
    --- dashboard.py
    +++ dashboard.py
    @@ ... @@
     import streamlit as st
    
    -PAGES = {
    -    "Simple-Screener": "This is the Simple-Screener page.",
    -    "Data-Explorer": "This is the Data-Explorer page."
    -}
    +def simple_screener_page():
    +    st.write("This is the Simple-Screener page.")
    +    # Add more code for Simple-Screener page here
    +
    +def data_explorer_page():
    +    st.write("This is the Data-Explorer page.")
    +    # Add more code for Data-Explorer page here
    +
    +PAGES = {
    +    "Simple-Screener": simple_screener_page,
    +    "Data-Explorer": data_explorer_page
    +}
    
     selection = st.sidebar.radio("Navigation:", list(PAGES.keys()))
    -page = PAGES[selection]
    -st.write(page)
    +page_function = PAGES[selection]
    +page_function()
    ```

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,19 @@
 import streamlit as st
 
+def simple_screener_page():
+    st.write("This is the Simple-Screener page.")
+    # Add more code for Simple-Screener page here
+
+def data_explorer_page():
+    st.write("This is the Data-Explorer page.")
+    # Add more code for Data-Explorer page here
+
 PAGES = {
-    "Simple-Screener": "This is the Simple-Screener page.",
-    "Data-Explorer": "This is the Data-Explorer page."
+    "Simple-Screener": simple_screener_page,
+    "Data-Explorer": data_explorer_page
 }
 
 selection = st.sidebar.radio("Navigation:", list(PAGES.keys()))
-page = PAGES[selection]
-st.write(page)
+page_function = PAGES[selection]
+page_function()
 
