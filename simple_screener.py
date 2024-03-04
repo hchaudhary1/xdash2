bb305d4b917675df75bd900ed61803b18cfc9bb1
Hassan <hassan789@gmail.com>
2024-03-03 17:37:47 -0800

    format code

--- simple_screener.py
+++ simple_screener.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 import uuid
 
+
 def log_scale_slider(label, start, end, key=None):
     """
     Creates a log-scaled range slider with direct float values, adjusting for negative start values.
@@ -31,7 +32,9 @@ def log_scale_slider(label, start, end, key=None):
     log_labels = [f"{value:.2f}" for value in log_values_unshifted]
 
     # Create a dictionary to map the labels back to the original values
-    label_to_value = {label: value for label, value in zip(log_labels, log_values_unshifted)}
+    label_to_value = {
+        label: value for label, value in zip(log_labels, log_values_unshifted)
+    }
 
     default_labels = (log_labels[0], log_labels[-1])
 
@@ -40,13 +43,14 @@ def log_scale_slider(label, start, end, key=None):
         label,
         options=log_labels,
         value=default_labels,  # Default to full range or specified default
-        key=key  # Pass the key parameter to the select_slider
+        key=key,  # Pass the key parameter to the select_slider
     )
 
     # Map the selected labels back to their original values
     selected_values = [label_to_value[label] for label in selected_labels]
 
     return selected_values[0], selected_values[1]
+
 
 def create_custom_df_column(unique_id):
     """
@@ -83,15 +87,19 @@ def create_custom_df_column(unique_id):
 
     # Use the unique_id as part of the key for each widget
     with col1:
-        selected_interval = st.selectbox("Time range:", interval, key=f"interval_{unique_id}")
+        selected_interval = st.selectbox(
+            "Time range:", interval, key=f"interval_{unique_id}"
+        )
     with col2:
         selected_era = st.selectbox("When:", era, key=f"era_{unique_id}")
     with col3:
-        selected_category = st.selectbox("Metric:", category, key=f"category_{unique_id}")
+        selected_category = st.selectbox(
+            "Metric:", category, key=f"category_{unique_id}"
+        )
 
     # Construct and return the df_column
     return f"{selected_category}_{selected_era}_{selected_interval}"
-    
+
 
 ## PAGE STREAMLIT START ##
 def simple_screener_page():
@@ -101,9 +109,9 @@ def simple_screener_page():
     st.write("## 1. Choose your filters:")
 
     # Initialize or increment the list of unique identifiers for filters
-    if 'filter_ids' not in st.session_state:
+    if "filter_ids" not in st.session_state:
         st.session_state.filter_ids = [str(uuid.uuid4())]
-    if 'sort_id' not in st.session_state:
+    if "sort_id" not in st.session_state:
         st.session_state.sort_id = [str(uuid.uuid4())]
 
     # Lists to store the settings for all filters
@@ -118,25 +126,28 @@ def simple_screener_page():
         # Display and collect the custom df_column selection
         custom_df_column = create_custom_df_column(unique_id)
         all_custom_df_columns.append(custom_df_column)
-        
+
         # Get the min and max for the selected df_column
         column_min = filtered_df[custom_df_column].min()
         column_max = filtered_df[custom_df_column].max()
 
         # Display and collect the log scale slider selection for each filter
         selected_range = log_scale_slider(
-            label=f'Select a range of values for {custom_df_column}',
+            label=f"Select a range of values for {custom_df_column}",
             start=column_min,
             end=column_max,
-            key=f"slider_{unique_id}"  # Ensure each slider has a unique key
+            key=f"slider_{unique_id}",  # Ensure each slider has a unique key
         )
         all_selected_ranges.append(selected_range)
 
         # Apply the filter to the filtered_df
-        filtered_df = filtered_df[(filtered_df[custom_df_column] >= selected_range[0]) & (filtered_df[custom_df_column] <= selected_range[1])]
+        filtered_df = filtered_df[
+            (filtered_df[custom_df_column] >= selected_range[0])
+            & (filtered_df[custom_df_column] <= selected_range[1])
+        ]
 
     # Button to add a new filter
-    if st.button('Add another filter'):
+    if st.button("Add another filter"):
         # Append a new unique identifier for the new filter
         st.session_state.filter_ids.append(str(uuid.uuid4()))
         st.rerun()  # Force a rerun of the app to immediately reflect the change
@@ -154,7 +165,7 @@ def simple_screener_page():
 
     # Display the filtered DataFrame or a summary
     st.write("## 3. Filtered List:")
-    
+
     # Reset the index of the DataFrame and then display it without the index column
     sorted_df = filtered_df.sort_values(by=custom_df_column, ascending=False)
     st.dataframe(sorted_df.reset_index(drop=True), use_container_width=True)
